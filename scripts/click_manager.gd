f8c7bba2c57cc5a186a8c7971e970baaf01eed82
Coleman DuPlessie <coleyduplessie@gmail.com>
2025-04-05 18:14:00 -0400

    Finished adding context to the tower context window

--- scripts/click_manager.gd
+++ scripts/click_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var tower_menu: Node2D
 @export var fog_of_war: TileMapLayer
+@export var tiles: TileMapLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +12,8 @@ func check_for_opening_tower_menu(mouse_pos: Vector2) -> void:
 	var snap = Vector2(floor(mouse_pos[0]/16+0.5)*16, floor(mouse_pos[1]/16+0.5)*16)
 	if tower_menu.size == 0: # The menu is closed
 		if fog_of_war.check_duplo_revealed(snap): # The place we want to build is not obscured by fog
-			# TODO query buildable tiles
-			tower_menu.open_at(snap)
+			if tiles.check_duplo_exists(snap): # The place we want to build is currently ground tiles
+				tower_menu.open_at(snap)
 	elif abs(tower_menu.global_position[0] - mouse_pos[0]) > 32 or abs(tower_menu.global_position[1] - mouse_pos[1]) > 32:
 		tower_menu.disappear()
 
