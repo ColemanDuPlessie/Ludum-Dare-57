976b661432f5b5789e81f7cb34c51b93857738c1
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-04-06 17:36:05 -0500

    Add health UI

--- scripts/resource_ui.gd
+++ scripts/resource_ui.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var health_container: Control = get_node("Health")
+
+var heart_icon = ResourceLoader.load("res://art/heart.tres")
+var heart_empty_icon = ResourceLoader.load("res://art/heart_empty.tres")
+
 var enabled = false
 
 func _ready() -> void:
@@ -13,6 +18,14 @@ func _process(delta: float) -> void:
 		position = Static.fixed_lerp(position, Vector2(0, 0), 4, delta)
 	else:
 		position = Static.fixed_lerp(position, Vector2(-100, 0), 4, delta)
+
+	for index in range(health_container.get_child_count()):
+		var icon: TextureRect = health_container.get_child(index)
+
+		if Static.health >= index + 1:
+			icon.texture = heart_icon
+		else:
+			icon.texture = heart_empty_icon
 
 func disable():
 	enabled = false
