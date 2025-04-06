2640ac67264366cb6e9c1d62dd531a6f4fb344b5
Coleman DuPlessie <coleyduplessie@gmail.com>
2025-04-06 01:23:48 -0400

    Made fog fade out slowly

--- scripts/fog_of_war.gd
+++ scripts/fog_of_war.gd
@@ -2,6 +2,9 @@ extends TileMapLayer
 
 const DUPLO_LOC_DELTAS = [Vector2(0, 0), Vector2(-1, 0), Vector2(0, -1), Vector2(-1, -1)]
 
+var fading_tiles = {} # Dict of Vector2is corresponding to tiles and timers for how long until they cease to exist.
+const FADE_TIMES = [0.8, 0.6, 0.45, 0.3, 0.15]
+
 func _on_drill_spawned(player):
 	player.moving_to_tile.connect(_on_player_moving_to_tile)
 	
@@ -15,6 +18,27 @@ func check_duplo_revealed(loc: Vector2) -> bool:
 			return false
 	return true
 
+func begin_fading_cell(loc: Vector2) -> void:
+	if not fading_tiles.has(loc):
+		fading_tiles[loc] = FADE_TIMES[Static.PLAYER_RADAR_LEVEL]
+
+func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
+	return fading_tiles.has(Vector2(coords))
+
+func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
+	tile_data.modulate = Color(1, 1, 1, fading_tiles[Vector2(coords)]/FADE_TIMES[Static.PLAYER_RADAR_LEVEL])
+
+func _process(delta: float) -> void:
+	if len(fading_tiles) > 0:
+		notify_runtime_tile_data_update()
+	for tile in fading_tiles.keys():
+		var remaining = fading_tiles[tile] - delta
+		if remaining <= 0:
+			erase_cell(tile)
+			fading_tiles.erase(tile)
+		else:
+			fading_tiles[tile] = remaining
+
 func reveal_from(loc: Vector2) -> void:
 	var min_x = loc.x - floor(sqrt(Static.MAX_REVEAL_RANGE))
 	var max_x = loc.x + floor(sqrt(Static.MAX_REVEAL_RANGE))
@@ -25,4 +49,4 @@ func reveal_from(loc: Vector2) -> void:
 		for y in range(min_y, max_y+1):
 			if (x-loc.x)**2 + (y-loc.y)**2 <= Static.MAX_REVEAL_RANGE:
 				if get_cell_atlas_coords(Vector2(x, y)) != Vector2i(-1, -1):
-					erase_cell(Vector2(x, y)) # TODO we can make this fade away instead of just pop out of existence; it'll look much cooler.
+					begin_fading_cell(Vector2(x, y))
